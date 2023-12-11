270c0f8298ee3642cb90a642885bfa829d323455
Bjørn Lie Rapp <90246213+Bjoern-Rapp@users.noreply.github.com.>
2023-12-11 11:18:58 +0100

    Add function "move gcs folder"

--- src/ssb_libtest7/functions.py
+++ src/ssb_libtest7/functions.py
@@ -5,31 +5,13 @@ https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
 
 """
 
+from dapla import FileClient
 
-def example_function(number1: int, number2: int) -> str:
-    """Compare two integers.
-
-    This is merely an example function can be deleted. It is used to show and test generating
-    documentation from code, type hinting, testing, and testing examples
-    in the code.
+fs = FileClient.get_gcs_file_system()
 
 
-    Args:
-        number1: The first number.
-        number2: The second number, which will be compared to number1.
-
-    Returns:
-        A string describing which number is the greatest.
-
-    Examples:
-        Examples should be written in doctest format, and should illustrate how
-        to use the function.
-
-        >>> example_function(1, 2)
-        1 is less than 2
-
-    """
-    if number1 < number2:
-        return f"{number1} is less than {number2}"
-
-    return f"{number1} is greater than or equal to {number2}"
+def move_gcs_folder(from_folder: str, to_folder: str):
+    """Move all files inn a given gcs "folder" """
+    for from_path in fs.ls(from_folder):
+        to_path = f"{to_folder}/{from_path.split('/')[-1]}"
+        fs.cp(from_path, to_path)
